8f92dddf2a3828b280dabe09fae96caed4e74434
= <=>
2023-05-26 21:57:41 -0500

    Cambios viernes 26-May

--- hola.py
+++ hola.py
@@ -1,1 +1,8 @@
-print("Hola mundo")
+#Declaración de variables
+nombre = input("Ingrese su nombre:")
+edad = float(input("Ingrese la edad:"))
+
+dato = str(edad)
+
+print('Usuario:', nombre, 'Edad:', edad)
+print("El cuadrado de la edad es:", edad ** 2)
