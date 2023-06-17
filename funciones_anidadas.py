1ad55b9fed03ed66709d3fbc07f34401cfa0def9
= <=>
2023-06-16 21:25:24 -0500

    Version 2

--- funciones_anidadas.py
+++ funciones_anidadas.py
@@ -4,7 +4,13 @@ def primera(datos):
     def segunda():
         for n in datos:
             print(n)
+        
+        def tercera():
+            print("Hola desde la tercera función")
+        
+        tercera()    
     
     segunda()
+    
 
 primera([2, 4, 6, 8])
