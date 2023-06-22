6ce95d520b4c90343236d1933c1635610c732489
= <=>
2023-06-21 21:30:54 -0500

    Primera version

---
+++ logica.py
@@ -0,0 +1,35 @@
+es_amigo = True
+es_familiar = True
+
+"""
+if es_amigo and es_familiar:
+    print('Es amigo y familiar')
+elif es_amigo:
+    print('Es amigo')
+elif es_familiar:
+    print('Es familiar')
+else:
+    print('No es amigo ni familiar')
+
+#Operador ternario
+# condicion_verdadera if condicion else condicion falsa
+mensaje = "Es amigo y familiar" if (es_amigo and es_familiar) else "No es amigo"
+print(mensaje)
+"""
+
+a = [1, 2, 3]
+b = [1, 2, 3]
+print(True == 1)
+print('' == 1)
+print([] == 1)
+print(10 == 10.0)
+print(a == b)
+print(' ')
+print(True is 1)
+print('' is 1)
+print([] is 1)
+print(10 is 10.0)
+print(a is b)
+
+
+
