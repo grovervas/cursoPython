eaf9deecdad6d3239583c761033f8c615d93f990
= <=>
2023-06-23 20:09:53 -0500

    version 1

---
+++ break_continue_pass.py
@@ -0,0 +1,16 @@
+list = [1, 2, 3, 4, 5]
+
+for item in list:
+    #luego agrego el codigo
+    pass
+
+n = 1
+while n <= len(list):
+    print(n)  
+    n += 1  
+    if n == 3:
+        continue
+    print('No es 3')
+
+    
+    
