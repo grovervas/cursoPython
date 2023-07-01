dacaf4d83bd136cfd8a053f8de6d1cc3a34489a0
= <=>
2023-06-30 19:45:11 -0500

    Version 1

--- POO/poo.py
+++ POO/poo.py
@@ -9,3 +9,16 @@ class Persona:
         print('Nombre: ', self.nombre)
         print('Edad: ', self.edad)
         print('Móvil: ', self.movil)
+
+#Instancias de la clase Persona
+per1 = Persona('Grover', 40, 999123456) 
+per2 = Persona('Pablo', 29, 987654321)
+per3 = Persona('Ana', 25, 678987654)
+per4 = Persona(None, None, None)
+
+per2.getDatos()
+print('='*10)
+per1.getDatos()
+print('='*10)
+per3.getDatos()
+print('='*10)
